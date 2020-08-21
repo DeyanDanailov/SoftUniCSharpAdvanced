ea472fea7fa73a3a21225517fa2faaf42f945c00
DeyanDanailov <stormbringer_ss@abv.bg>
2020-08-21 13:37:32 +0300

    max min task done

---
+++ tasks/03.MaxAndMinElement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaxAndMinElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfQueries = int.Parse(Console.ReadLine());
+            var numbers = new Stack<int>();
+            for (int i = 0; i < numOfQueries; i++)
+            {
+                var command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int action = command[0];
+                if (action == 1)
+                {
+                    numbers.Push(command[1]);
+                }
+                else if (action == 2)
+                {
+                    if (numbers.Any())
+                    {
+                        numbers.Pop();
+                    }
+                }
+                else if (action == 3)
+                {
+                    if (numbers.Any())
+                    {
+                        Console.WriteLine(numbers.Max()); 
+                    }
+                }
+                else if (action == 4)
+                {
+                    if (numbers.Any())
+                    {
+                        Console.WriteLine(numbers.Min());
+                    }
+                }
+
+            }
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
