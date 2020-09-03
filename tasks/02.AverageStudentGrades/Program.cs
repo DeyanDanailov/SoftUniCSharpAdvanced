f9af070fb606a43aad975d0563018412ffa35cf6
DeyanDanailov <stormbringer_ss@abv.bg>
2020-09-03 15:58:27 +0300

    average student task done

--- tasks/02.AverageStudentGrades/Program.cs
+++ tasks/02.AverageStudentGrades/Program.cs
@@ -12,20 +12,20 @@ namespace _02.AverageStudentGrades
         {
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             var n = int.Parse(Console.ReadLine());
-            var allStudents = new Dictionary<string, List<double>>();
+            var allStudents = new Dictionary<string, List<decimal>>();
             for (int i = 0; i < n; i++)
             {
                 var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
                 var name = input[0];
-                var grade = double.Parse(input[1]);
-                if (!allStudents.TryAdd(name, new List<double>() {grade}))
+                var grade = decimal.Parse(input[1]);
+                if (!allStudents.TryAdd(name, new List<decimal>() {grade}))
                 {
                     allStudents[name].Add(grade);
                 }
             }
             foreach (var item in allStudents)
             {
-                Console.WriteLine($"{item.Key} -> {String.Join(' ',item.Value):f2} (avg: {item.Value.Average():f2})");
+                Console.WriteLine($"{item.Key} -> {String.Join(' ',item.Value.Select(x=>x.ToString("F2")))} (avg: {item.Value.Average():f2})");
             }
         }
     }
