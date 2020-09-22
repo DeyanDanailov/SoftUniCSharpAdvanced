021fb444d58c35ebcd7b9e04330bcd9877f1df6b
DeyanDanailov <stormbringer_ss@abv.bg>
2020-09-22 16:06:00 +0300

    line numbers task done

---
+++ tasks/02.LineNumbers/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _02.LineNumbers
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadLines("text.txt").ToArray();
+            
+            for (int i = 0; i < text.Length; i++)
+            { 
+                var letters = 0;
+                var puncts = 0;
+                foreach (var symbol in text[i])
+                {
+                    if (Char.IsLetter(symbol))
+                    {
+                        letters++;
+                    }
+                    if (char.IsPunctuation(symbol))
+                    {
+                        puncts++;
+                    }
+                }
+                File.AppendAllText("result.txt", $"Line {i +1}: {text[i]} ({letters})({puncts})\n");
+            }
+        }
+    }
+}
