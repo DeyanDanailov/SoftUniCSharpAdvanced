bc89933513256e49aaced57a88824a3752820bcd
DeyanDanailov <stormbringer_ss@abv.bg>
2020-09-22 14:12:32 +0300

    task done

---
+++ tasks/03.WordCount/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var readerOfWords = new StreamReader("words.txt");
+            var currentWords = readerOfWords.ReadToEnd().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            
+            using var readerOfText = new StreamReader("text.txt");
+            var line = readerOfText.ReadLine();
+            var currentWordsOfText = new List<string>();
+            var result = new Dictionary<string, int>();
+            while (line != null)
+            {
+                currentWordsOfText = Regex.Split(line, @"[\s+,.?!-]+", RegexOptions.IgnoreCase,
+                                    TimeSpan.FromMilliseconds(500)).ToList();
+                for (int i = 0; i < currentWordsOfText.Count; i++)
+                {
+                    var currentWord = currentWordsOfText[i].ToLower();
+                    if (currentWords.Contains(currentWord))
+                    {
+                        if (!result.ContainsKey(currentWord))
+                        {
+                            result[currentWord] = 0;
+                            result[currentWord]++;
+                        }
+                        else
+                        {
+                            result[currentWord]++;
+                        }
+                    }
+                }
+                line = readerOfText.ReadLine();
+            }
+            
+           
+            
+            result = result.OrderByDescending(x => x.Value).ToDictionary(a => a.Key, b => b.Value);
+            using var writer = new StreamWriter("output.txt", true);
+            foreach (var word in result)
+            {
+                writer.WriteLine($"{word.Key} - {word.Value}");
+                writer.Flush();
+            }
+        }
+    }
+}
---
+++ tasks/04.MergeFiles/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _04.MergeFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var reader1 = new StreamReader("input1.txt");
+            using var reader2 = new StreamReader("input2.txt");
+            var writer = new StreamWriter("output.txt");
+            var line1 = reader1.ReadLine();
+            var line2 = reader2.ReadLine();
+            while (true)
+            {
+                if (line1 == null && line2 == null)
+                {
+                    break;
+                }
+                if (line1 != null)
+                {
+                    writer.WriteLine(line1);
+                }
+                if (line2 != null)
+                {
+
+                    writer.WriteLine(line2);
+                }
+
+                line1 = reader1.ReadLine();
+                line2 = reader2.ReadLine();
+
+            }
+            writer.Flush();
+        }
+    }
+}
