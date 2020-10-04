18c6932bebbdba5ea6468007c904473895925b44
DeyanDanailov <stormbringer_ss@abv.bg>
2020-10-04 17:12:12 +0300

    car salesman task done

--- tasks/DefiningClasses/StartUp.cs
+++ tasks/DefiningClasses/StartUp.cs
@@ -10,7 +10,7 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            //06.task
+            //05.task
             //string date1 = Console.ReadLine();
             //string date2 = Console.ReadLine();
 
@@ -18,35 +18,26 @@ namespace DefiningClasses
 
             //Console.WriteLine(dateModifier.CalculateDifference(date1, date2));
 
-            // 07.task
-            //var n = int.Parse(Console.ReadLine());
-            //var car = new Car();
-            //var allCars = new List<Car>();
-            //for (int i = 0; i < n; i++)
-            //{
-            //    var info = Console.ReadLine();
-            //    car = car.CollectCarInfo(info);
-            //    allCars.Add(car);
-            //}
-            //var command = Console.ReadLine();
-            //while (command!="End")
-            //{
-            //    car.DriveCar(command, allCars);
-            //    command = Console.ReadLine();
-            //}
-            //car.PrintCars(allCars);
 
-            int n = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
             var car = new Car();
-            var cars = new HashSet<Car>();
+            var allCars = new List<Car>();
             for (int i = 0; i < n; i++)
             {
-                var input = Console.ReadLine();                
-                var curentCar = car.CollectCarInfo(input);
-                cars.Add(curentCar);
+                var info = Console.ReadLine();
+                car = car.CollectCarInfo(info);
+                allCars.Add(car);
             }
-            var cargoFilter = Console.ReadLine();
-            car.PrintCarsDependingOnCargoFilter(cars, cargoFilter);
+            var command = Console.ReadLine();
+            while (command != "End")
+            {
+                car.DriveCar(command, allCars);
+                command = Console.ReadLine();
+            }
+            car.PrintCars(allCars);
+
+            
+            
         }
     }
 }
