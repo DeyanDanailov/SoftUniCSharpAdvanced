997fcef04aa68b2e3bfcbb941641e0727c0dd1a6
DeyanDanailov <stormbringer_ss@abv.bg>
2020-10-05 14:05:11 +0300

    streams homework

--- tasks/04.CopyBinaryFile/StartUp.cs
+++ tasks/04.CopyBinaryFile/StartUp.cs
@@ -3,14 +3,31 @@ using System.IO;
 
 namespace _04.CopyBinaryFile
 {
-    class StartUp
+
+    using System;
+    using System.IO;
+
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            using var stream = new FileStream("copyMe.png", FileMode.OpenOrCreate);
-            using var copied = new FileStream("copy.png", FileMode.OpenOrCreate);
-            stream.CopyTo(copied);
-           
+            const int DEF_SIZE = 4096;
+            using FileStream reader = new FileStream("../../../copyMe.png", FileMode.Open);
+            using FileStream writer = new FileStream("../../../copied.png", FileMode.Create);
+
+            byte[] buffer = new byte[DEF_SIZE];
+
+            while (reader.CanRead)
+            {
+                int bytesRead = reader.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                writer.Write(buffer, 0, buffer.Length);
+            }           
         }
+
     }
 }
